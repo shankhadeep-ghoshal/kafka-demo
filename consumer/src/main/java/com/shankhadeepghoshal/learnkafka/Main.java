506d927d56989c759c70a325b8ff38bef720540c
Shankhadeep-Ghoshal <shankhadeepghoshal1996@gmail.com>
2022-11-16 19:09:40 +0530

    Added modules

---
+++ consumer/src/main/java/com/shankhadeepghoshal/learnkafka/Main.java
@@ -0,0 +1,5 @@
+package com.shankhadeepghoshal.learnkafka;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/java/com/shankhadeepghoshal/Main.java
+++ src/main/java/com/shankhadeepghoshal/Main.java
@@ -1,4 +1,4 @@
-package com.shankhadeepghoshal.learnkafka;
+package com.shankhadeepghoshal;
 
 public class Main {
     public static void main(String[] args) {
